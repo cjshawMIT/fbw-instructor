ac166ec3b30f6984a217e525f49a22b657910140
cjshaw <cjshaw@mit.edu>
2017-03-21 16:44:06 -0400

    create fake default target question for student results

--- web/src/routes/Dashboard/views/ResultsView.js
+++ web/src/routes/Dashboard/views/ResultsView.js
@@ -102,7 +102,7 @@ class ResultsView extends Component {
             {_.map(props.viewData.results, studentResult => {
               return (<StudentLink key={studentResult.takingAgentId} className="students-list__item"
                                   studentResult={studentResult}
-                                  selectedQuestion={props.currentTarget}
+                                  selectedQuestion={{id: 0}}
                                   currentDirectiveIndex={props.currentDirectiveIndex}
                                   onSelectResult={this.props.onSelectMissionResult}/>)
             })}
